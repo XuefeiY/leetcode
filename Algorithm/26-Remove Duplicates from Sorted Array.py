d96dcf8a001b3fd3dfe20703051789974d0334af
Xuefei Yang <yangxuefei@hotmail.com>
2017-10-27 14:25:05 -0400

    Update 26-Remove Duplicates from Sorted Array.py

--- Algorithm/26-Remove Duplicates from Sorted Array.py
+++ Algorithm/26-Remove Duplicates from Sorted Array.py
@@ -1,11 +1,8 @@
 """
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
-
 Do not allocate extra space for another array, you must do this in place with constant memory.
-
 For example,
 Given input array nums = [1,1,2],
-
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
 It doesn't matter what you leave beyond the new length.
 """
@@ -57,7 +54,7 @@ class Solution:
         j = 0
 
         for i in range(1, len(A)):
-            if A[i] != A[j]:    # A[newTail] is the element to be compared, A[i] is the current element during traverse
+            if A[i] != A[j]:    # A[j] is the element to be compared, A[i] is the current element during traverse
                 j += 1
                 A[i], A[j+1] = A[j+1], A[i]]
 
